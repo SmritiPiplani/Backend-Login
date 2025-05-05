4de7a53726af3c4b63886bbeee58cd1db29380db
SmritiPiplani <smritipiplani16@gmail.com>
2025-05-05 10:20:02 +0530

    git commit -m "Update"

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { register, sendOtp, verifyOtp, login, logout } from '../controllers/AuthController.js';
+import { register, sendOtp, verifyOtp, login, logout ,getAllUsers} from '../controllers/AuthController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 import { authorizeRoles } from '../middlewares/roleMiddleware.js';
-import { protectedRoute } from '../controllers/AuthController.js';
+
 const router = express.Router();
 
 router.post('/register', register);
@@ -10,7 +10,8 @@ router.post('/send-otp', sendOtp);
 router.post('/verify-otp', verifyOtp);
 router.post('/login', login);
 router.post('/logout', logout);
-router.get('/user', authMiddleware, authorizeRoles('user', 'admin'), protectedRoute);
-router.get('/admin', authMiddleware, authorizeRoles('admin'), protectedRoute);
+
+router.get('/users', authMiddleware, authorizeRoles('admin'), getAllUsers);
+
 export default router;
  
